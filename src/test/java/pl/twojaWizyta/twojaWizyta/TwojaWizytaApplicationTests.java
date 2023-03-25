d727289f8f17f27b5c64998b16d72284a28c600b
ula <uaromaniuk@gmail.com>
2023-03-25 10:52:05 +0100

    init commit

---
+++ src/test/java/pl/twojaWizyta/twojaWizyta/TwojaWizytaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.twojaWizyta.twojaWizyta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwojaWizytaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
